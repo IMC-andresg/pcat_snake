9381625c8a79b139d9a49b3b6364e8b8306eae4f
Amit.Roy <Amit.Roy@ingrammicro.com>
2021-06-28 01:02:05 +0530

    Code commit for MAP-455 - Snake Tool - Fill data in country boxes inside APS SKUs Tab

--- generators/csp_generator.py
+++ generators/csp_generator.py
@@ -235,38 +235,55 @@ class CSPGenerator:
         # Consolidated APS tab
         for group in self.config['SKU_GROUPS']:
             om_new_filtered = self.om_new[self.om_new['Group'] == group].sort_values(by='Offer Display Name')
-            common_columns = {'License Type': self.config['SKU_GROUPS'][group]['License Type'], 'Ingram Group': self.config['SKU_GROUPS'][group]['Ingram Group'], 'Family': self.config['SKU_GROUPS'][group]['Family']}
+            license_type = self.config['SKU_GROUPS'][group]['License Type']
+            common_columns = {'License Type': license_type, 'Ingram Group': self.config['SKU_GROUPS'][group]['Ingram Group'], 'Family': self.config['SKU_GROUPS'][group]['Family']}
             for sku, sku_data in om_new_filtered[om_new_filtered['Parent/Child'] == 'Parent'].iterrows():
                 if sku_data['Duration'] in self.config['ALLOWED_DURATIONS'] or self.config['INCLUDE_LONG_TERM_SKUS']:
+                    # Calculating MAX seat count for Ingram
+                    max_im_seat_count = sku_data['Max Seat Count']
+                    if max_im_seat_count > 5000:
+                        max_im_seat_count = 5000
+                    #Rows for Parent Plan
                     sku_rel_id = sku
+                    country_specific_name =''
+                    #addon_ppc is only required to identify addon plan PPC value
+                    addon_ppc = ''
                     if sku_rel_id in self.loader.aps_skus_last['PPC Dup'].values:
                         aps_skus_last_name = self.loader.aps_skus_last[self.loader.aps_skus_last['PPC Dup'] ==  sku_rel_id].iloc[0]['Name for Ingram']
                     else:
-                        aps_skus_last_name = sku_data['Offer Display Name']
-                    self.aps_skus = self.aps_skus.append({'PPC Dup': sku,**common_columns, 'OfferID': sku,'Offer Name (From Microsoft Offers Matrix)':sku_data['Offer Display Name'],'Name for Ingram':sku_data['Offer Display Name'],'Old Offer Name':aps_skus_last_name, 'Parent SKU + RelationID': sku, 'Parent SKU': sku, 'Parent Name': sku_data['Offer Display Name'],'CMP Category': sku_data['CMP Category'], 'SKU In Last Month OM': sku_data['In Last Month OM'],'Included Amount':1,'Lower Limit, Min Seat Count':sku_data['Min Seat Count'],'Max Amount Ingram':sku_data['Max Seat Count'],'Subscription Term':sku_data['Duration'],'Name Change': sku_data['Name Change']}, ignore_index=True)
+                        aps_skus_last_name = sku_data['Offer Display Name']                    
+                    additional_column = {'PPC Dup': sku,**common_columns, 'OfferID': sku,'Offer Name (From Microsoft Offers Matrix)':sku_data['Offer Display Name'],'Name for Ingram':sku_data['Offer Display Name'],'Old Offer Name':aps_skus_last_name, 'Parent SKU + RelationID': sku, 'Parent SKU': sku, 'Parent Name': sku_data['Offer Display Name'],'CMP Category': sku_data['CMP Category'], 'SKU In Last Month OM': sku_data['In Last Month OM'],'Included Amount':1,'Lower Limit, Min Seat Count':sku_data['Min Seat Count'],'Max Amount Ingram':max_im_seat_count,'Subscription Term':sku_data['Duration'],'Name Change': sku_data['Name Change']}
+                    self.build_country_boxes('parent',additional_column,sku_rel_id,sku,addon_ppc,license_type)
                     if sku in list(dict.fromkeys(self.relations_current['ParentId'])):
                         addons_sku_list = list(dict.fromkeys(self.relations_current[self.relations_current['ParentId'] == sku]['ChildId']))
                         for relation_sku, relation_sku_data in self.relations_current[self.relations_current['ParentId'] == sku].sort_values(by=['ChildName', 'ChildId']).iterrows():
+                            #Rows for Addon Plan
                             if relation_sku_data['ChildId'] in self.config['COUNTRY_SPECIFIC_SKU']:
                                 country_specific_name = self.config['COUNTRY_SPECIFIC_SKU'][relation_sku_data['ChildId']]
                             else:
-                                country_specific_name = relation_sku_data['ChildId']
+                                country_specific_name = relation_sku_data['ChildName']
+                            print(country_specific_name)    
                             sku_rel_id = sku + relation_sku
                             if sku_rel_id in self.loader.aps_skus_last['PPC Dup'].values:
                                 aps_skus_last_name = self.loader.aps_skus_last[self.loader.aps_skus_last['PPC Dup'] ==  sku_rel_id].iloc[0]['Name for Ingram']
                             else:
                                 aps_skus_last_name = country_specific_name
-                            self.aps_skus = self.aps_skus.append({'PPC Dup': sku + relation_sku,**common_columns, 'OfferID': relation_sku_data['ChildId'],'Offer Name (From Microsoft Offers Matrix)':relation_sku_data['ChildName'],'Name for Ingram':country_specific_name,'Old Offer Name':aps_skus_last_name, 'RelationID': relation_sku, 'Parent SKU + RelationID': sku + relation_sku, 'Parent SKU': sku, 'Parent Name': sku_data['Offer Display Name'], 'CMP Category': self.om_new.loc[relation_sku_data['ChildId'], 'CMP Category'], 'SKU In Last Month OM': self.om_new.loc[relation_sku_data['ChildId'], 'In Last Month OM'], 'Relation In Last Month': relation_sku_data['Last Month'], 'Included Amount':0,'Lower Limit, Min Seat Count':sku_data['Min Seat Count'],'Max Amount Ingram':sku_data['Max Seat Count'],'Subscription Term':sku_data['Duration'],'Name Change': self.om_new.loc[relation_sku_data['ChildId'], 'Name Change']}, ignore_index=True)
+                            addon_ppc = sku + relation_sku                    
+                            additional_column = {'PPC Dup': sku + relation_sku,**common_columns, 'OfferID': relation_sku_data['ChildId'],'Offer Name (From Microsoft Offers Matrix)':relation_sku_data['ChildName'],'Name for Ingram':country_specific_name,'Old Offer Name':aps_skus_last_name, 'RelationID': relation_sku, 'Parent SKU + RelationID': sku + relation_sku, 'Parent SKU': sku, 'Parent Name': sku_data['Offer Display Name'], 'CMP Category': self.om_new.loc[relation_sku_data['ChildId'], 'CMP Category'], 'SKU In Last Month OM': self.om_new.loc[relation_sku_data['ChildId'], 'In Last Month OM'], 'Relation In Last Month': relation_sku_data['Last Month'], 'Included Amount':0,'Lower Limit, Min Seat Count':sku_data['Min Seat Count'],'Max Amount Ingram':max_im_seat_count,'Subscription Term':sku_data['Duration'],'Name Change': self.om_new.loc[relation_sku_data['ChildId'], 'Name Change']}
+                            self.build_country_boxes('addon',additional_column,sku_rel_id,sku,addon_ppc,license_type)                    
                             if relation_sku_data['ChildId'] in list(dict.fromkeys(self.relations_current['ParentId'])):
                                 for addon_to_addon_sku, addon_to_addon_data in self.relations_current[self.relations_current['ParentId'] == relation_sku_data['ChildId']].sort_values(by=['ChildName', 'ChildId']).iterrows():
+                                   #Rows for Addon to Addon Plan
                                    if addon_to_addon_data['ChildId'] not in addons_sku_list and self.config['REMOVE_ADDONS2ADDONS_IF_ADDON_EXISTS']:
-                                        addon_to_addon_offer_name = addon_to_addon_data['ChildName'] + " for " + addon_to_addon_data['ParentName']
+                                        addon_to_addon_offer_name = addon_to_addon_data['ChildName'] + " for " + country_specific_name
                                         sku_rel_id = sku + addon_to_addon_sku
                                         if sku_rel_id in self.loader.aps_skus_last['PPC Dup'].values:
                                             aps_skus_last_name = self.loader.aps_skus_last[self.loader.aps_skus_last['PPC Dup'] ==  sku_rel_id].iloc[0]['Name for Ingram']
                                         else:
-                                            aps_skus_last_name = addon_to_addon_offer_name
-                                        self.aps_skus = self.aps_skus.append({'PPC Dup': sku + addon_to_addon_sku,**common_columns, 'OfferID': addon_to_addon_data['ChildId'],'Offer Name (From Microsoft Offers Matrix)':sku_data['Offer Display Name'],'Name for Ingram':addon_to_addon_offer_name,'Old Offer Name':aps_skus_last_name, 'RelationID': addon_to_addon_sku, 'Parent SKU + RelationID': sku + addon_to_addon_sku, 'Parent SKU': sku, 'Parent Name': sku_data['Offer Display Name'], 'CMP Category': 'ADDON to ADDON', 'SKU In Last Month OM': self.om_new.loc[addon_to_addon_data['ChildId'], 'In Last Month OM'], 'Relation In Last Month': addon_to_addon_data['Last Month'],'Included Amount':0,'Lower Limit, Min Seat Count':sku_data['Min Seat Count'],'Max Amount Ingram':sku_data['Max Seat Count'],'Subscription Term':sku_data['Duration'], 'Name Change': self.om_new.loc[addon_to_addon_data['ChildId'], 'Name Change']}, ignore_index=True)
+                                            aps_skus_last_name = addon_to_addon_offer_name                                        
+                                        additional_column = {'PPC Dup': sku + addon_to_addon_sku,**common_columns, 'OfferID': addon_to_addon_data['ChildId'],'Offer Name (From Microsoft Offers Matrix)':addon_to_addon_data['ChildName'],'Name for Ingram':addon_to_addon_offer_name,'Old Offer Name':aps_skus_last_name, 'RelationID': addon_to_addon_sku, 'Parent SKU + RelationID': sku + addon_to_addon_sku, 'Parent SKU': sku, 'Parent Name': sku_data['Offer Display Name'], 'CMP Category': 'ADDON to ADDON', 'SKU In Last Month OM': self.om_new.loc[addon_to_addon_data['ChildId'], 'In Last Month OM'], 'Relation In Last Month': addon_to_addon_data['Last Month'],'Included Amount':0,'Lower Limit, Min Seat Count':sku_data['Min Seat Count'],'Max Amount Ingram':max_im_seat_count,'Subscription Term':sku_data['Duration'], 'Name Change': self.om_new.loc[addon_to_addon_data['ChildId'], 'Name Change']}
+                                        self.build_country_boxes('addon2addon',additional_column,sku_rel_id,sku,addon_ppc,license_type)
+                    
         self.cat_lic_type = CategoricalDtype(['Corporate','Education','Charity','Trial','Government'],ordered=True)
         self.cat_ingram_group = CategoricalDtype(['O365','EM+S','Windows','M365','D365','Trial'],ordered=True)
         self.aps_skus['License Type'] = self.aps_skus['License Type'].astype(self.cat_lic_type)
@@ -275,6 +292,64 @@ class CSPGenerator:
         self.aps_skus['Index'] = self.aps_skus.groupby(['License Type', 'Ingram Group']).cumcount()+1
         self.aps_skus.index = np.arange(1, len(self.aps_skus)+1)
 	
+    def build_country_boxes(self, plan_type, additional_column, sku_rel_id, sku, addon_ppc, license_type):
+        # Fill data for all Country Boxes
+        countries = self.config['COUNTRY_LIST']
+        country_values = {}
+        for country in countries:
+            if sku_rel_id in self.loader.aps_skus_last['PPC Dup'].values:
+                new_relation = 'NO'
+                country_value = self.country_data_check(self.loader.aps_skus_last[self.loader.aps_skus_last['PPC Dup'] ==  sku_rel_id].iloc[0][country])
+            else:
+                new_relation = 'YES'                
+                country_value = ''                                    
+                if self.country_availability.loc[sku,country] == 'YES':
+                    if license_type == 'Government':
+                        country_value = 'NO'
+                    else:
+                        if plan_type == "parent":
+                            country_value = 'Add If Available'                      
+                        elif plan_type == "addon":
+                            parent_data_check = self.aps_skus[(self.aps_skus['Parent SKU'] == sku) & (self.aps_skus['CMP Category'] !='ADDON') & (self.aps_skus['CMP Category'] !='ADDON to ADDON')].iloc[0][country]
+                            if parent_data_check in ['Existing In Country', 'Add If Available']: 
+                                country_value = 'Add If Available'
+                            else:
+                                country_value = 'NO'                    
+                        elif plan_type == "addon2addon":
+                            parent_data_check = self.aps_skus[self.aps_skus['PPC Dup'] == addon_ppc].iloc[0][country]
+                            if parent_data_check in ['Existing In Country', 'Add If Available']:
+                                country_value = 'Add If Available'
+                            else:
+                                country_value = 'NO'
+                else:
+                    country_value = 'NP'        
+            country_values[country] = country_value
+        self.aps_skus = self.aps_skus.append({**additional_column,'New Relation':new_relation,**country_values}, ignore_index=True)
+        
+    def country_data_check(self,country_data):
+        country_data_return=''
+        if country_data == 'Add If Available':
+            country_data_return = 'Existing In Country'
+        elif country_data == 'Delete':
+            country_data_return = 'NP'
+        elif country_data == 'Name Change':
+            country_data_return = 'Existing In Country'        
+        else:
+            country_data_return = country_data
+        return country_data_return    
+
+    def build_aps_skus_last(self):
+        # Fill data for Delete list in last month aps skus tab
+        countries = self.config['COUNTRY_LIST']
+        country_values = {}        
+        deleted_list = self.loader.om_del_this_month['OfferID'].values        
+        for deleted_sku in deleted_list:
+            for sku, sku_data in self.aps_skus_last[self.aps_skus_last.loc['OfferId',deleted_sku]]: 
+                for country in countries:
+                    deleted = 'YES'
+                    country_value = 'Delete'
+                    country_values[country] = country_value
+
     def build_connect_skus(self):
         # Consolidated Connect Tab
         for group in self.config['SKU_GROUPS']:
